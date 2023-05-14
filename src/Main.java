2b68cb7676653565c36dae6eb565ef25a249775b
HellHorseman <duke.sheffer@yandex.ru>
2023-05-14 17:21:55 +0400

    Add Task 3

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ import java.util.*;
 
 public class Main {
     private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-    private static final List<String> wolds = new ArrayList<>(List.of());
-    private static final List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+    private static final List<String> words = new ArrayList<>(List.of("We", "don`t", "need", "no", "education", "We", "don`t", "need", "no", "thought", "control"));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
 
     public List<Integer> getNums() {
         return nums;
     }
+
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -36,4 +38,19 @@ public class Main {
         }
         System.out.println();
     }
+
+    public static void task3() {
+        System.out.println("Задание 3");
+        Set<String> repeat = new HashSet<>();
+        Set<String> unique = new HashSet<>();
+        for (String word : words) {
+            if (!unique.add(word)) {
+                repeat.add(word);
+            }
+        }
+            System.out.println("Unique " + unique);
+            System.out.println("Repeats " + repeat);
+            System.out.println();
+    }
+
 }
